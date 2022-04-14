80ac6cf89732da4b2deb1b21c35fe55269c1f972
Syafiq-Madhi <20ftt1450@student.pb.edu.bn>
2022-04-14 12:54:05 +0800

    bahit was here inserted

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ import HelloWorld from './components/HelloWorld.vue'
         </div>
         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">phone number</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">+673 7211279</dd>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Bahit was here</dd>
         </div>
         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">ID</dt>
