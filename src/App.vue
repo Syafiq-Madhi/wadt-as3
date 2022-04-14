19128d4a778b4fd06f0cf16db724fc0a270397fc
Syafiq-Madhi <20ftt1450@student.pb.edu.bn>
2022-04-14 08:00:37 +0800

    add vue config js

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,33 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <h1>Md Syafiq Madhi Bin Matali</h1>
   <img src="./assets/f1.jpg" alt="" class="center">
+  <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+    <div class="px-4 py-5 sm:px-6">
+      <h3 class="text-lg leading-6 font-medium text-gray-900">Applicant Information</h3>
+      <p class="leading-6 text-gray-900">Personal details and application.</p>
+    </div>
+    <div class="border-t border-gray-200">
+      <dl>
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Full name</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Md Syafiq Madhi bin Matali</dd>
+        </div>
+        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">phone number</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">+673 7211279</dd>
+        </div>
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Email address</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">20FTT1450@dtudent.pb.edu.bn</dd>
+        </div>
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">About</dt>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">I am currently studying Web Development in Politeknik Brunei. I have been interested in making website since i was 18 years old. What i like the most in web development is to see the fruit of all my hard work into coding turn into a working and beautiful website. I am looking forward to expanding my knowledge on developing more interesting manners of website.</dd>
+        </div>
+      </dl>
+    </div>
+  </div>
 </template>
 
 <style>
@@ -18,4 +43,11 @@ import HelloWorld from './components/HelloWorld.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
+
+img {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+
 </style>
